28b9989fb776b0570c5642a8a0b1916e099a1fae
Sergey Akopkokhyants <akserg@gmail.com>
2013-02-07 10:38:50 +0200

    Check tests

--- test/run.sh
+++ test/run.sh
@@ -2,6 +2,23 @@
 
 set -e
 
+###############
+# Type Analysis
+###############
+
+echo
+echo "dart_analyzer lib/*.dart"
+
+results=$(dart_analyzer lib/*.dart 2>&1 1>/dev/null)
+
+echo "$results"
+
+#if [ -n "$results" ]; then
+#    exit 1
+#else
+#    echo "Passed analysis."
+#fi
+
 ############
 # Unit Tests
 ############

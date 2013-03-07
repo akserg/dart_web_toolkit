db39434afd13f557d4304ec14b64ad561e55cb22
Sergey Akopkokhyants <akserg@gmail.com>
2013-03-07 07:49:32 +0200

    Testing

--- test/run.sh
+++ test/run.sh
@@ -24,6 +24,8 @@ fi
 echo "DumpRenderTree test/index.html"
 results=`DumpRenderTree test/index.html 2>&1`
 
+echo "$results"
+
 echo "$results" | grep CONSOLE
 
 echo $results | grep 'unittest-suite-success' >/dev/null

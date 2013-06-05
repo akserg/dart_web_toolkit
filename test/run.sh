a7306b6cf0d793ca451a7f5821e113621d7cdbea
Sergey Akopkokhyants <akserg@gmail.com>
2013-06-05 09:38:08 +0200

    Fixed test/run.sh script

--- test/run.sh
+++ test/run.sh
@@ -6,32 +6,31 @@ set -e
 # Type Analysis
 ###############
 
-#echo
-#echo "Analyse code..."
+echo "Analyse code..."
 
-#output=$(dart_analyzer --extended-exit-code lib/*.dart 2>&1)
-#results=$?
+output=$(dart_analyzer --extended-exit-code lib/*.dart 2>&1)
+results=$?
 
-#if [ "$results" -ne 0 ]; then
-#    exit 1
-#else
-#   echo "Passed analysis."
-#fi
+if [ "$results" -ne 0 ]; then
+    exit 1
+else
+   echo "Passed analysis."
+fi
 
 ############
 # Unit Tests
 ############
 
-echo "Run tests..."
+#echo "Run tests..."
 
-results=$(DumpRenderTree test/index.html 2>&1)
+#results=$(DumpRenderTree test/index.html 2>&1)
 
-echo "------------------"
-echo "$results" | grep FAILED
-echo "------------------"
+#echo "------------------"
+#echo "$results" | grep FAILED
+#echo "------------------"
 
-if grep -q "Exception: Some tests failed." <<<$results; then
-  exit 1
-else
-  echo "Passed tests."
-fi
+#if grep -q "Exception: Some tests failed." <<<$results; then
+#  exit 1
+#else
+#  echo "Passed tests."
+#fi

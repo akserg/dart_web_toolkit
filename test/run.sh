2c49031c0e9e8fd2f24583f7f7c0a3a16fb5d400
Sergey Akopkokhyants <akserg@gmail.com>
2013-06-05 08:41:22 +0200

    Fixed test/run.sh script

--- test/run.sh
+++ test/run.sh
@@ -7,7 +7,7 @@ set -e
 ###############
 
 echo
-echo "dart_analyzer --extended-exit-code lib/*.dart"
+echo "Analyse code..."
 
 output=$(dart_analyzer --extended-exit-code lib/*.dart 2>&1)
 results=$?
@@ -18,28 +18,20 @@ else
     echo "Passed analysis."
 fi
 
-#results=$(dart_analyzer --extended-exit-code lib/*.dart)
-#
-#if [ -n "$results" ]; then
-#    exit 1
-#else
-#    echo "Passed analysis."
-#fi
-
 ############
 # Unit Tests
 ############
 
-#echo "DumpRenderTree test/index.html"
-#results=`DumpRenderTree test/index.html 2>&1`
+echo "Run tests..."
+results="DumpRenderTree test/index.html 2>&1"
 
-#echo "------------------"
-#echo "$results"
-#echo "------------------"
+echo "------------------"
+echo "$results"
+echo "------------------"
 
-#echo "$results" | grep CONSOLE
+echo "$results" | grep CONSOLE
 
-#echo $results | grep 'unittest-suite-success' >/dev/null
+echo $results | grep 'unittest-suite-success' >/dev/null
 
-#echo $results | grep -v 'Exception: Some tests failed.' >/dev/null
+echo $results | grep -v 'Exception: Some tests failed.' >/dev/null
 
